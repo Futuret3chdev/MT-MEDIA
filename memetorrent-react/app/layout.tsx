4569db4413509e85862e768105f12c01e7c73e65
futuret3chdev <jason.c@futuret3ch.com.au>
2026-08-18 15:45:06 +1000

    Remove Nova Poker from the site
    Drop the catalog entry, 18+ listing, and cover.

--- memetorrent-react/app/layout.tsx
+++ memetorrent-react/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-build="2026-08-18-nova-poker">
+    <html lang="en" data-build="2026-08-18-no-nova">
       <head>
         {/* Font Awesome for social icons in original brand colors */}
         <link 
